edf69b367d918964407d07c744aa548280f4594e
Ivan Storck <ivanoats@gmail.com>
2014-05-08 12:47:49 -0700

    Add basic CasperJS tests https://app.asana.com/0/11169835259592/12181981905060

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,48 @@
 module.exports = function(grunt) {
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-casper');
   
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'server.js']
+    },
+    express: {
+      options: {
+        // Override defaults here
+      },
+      dev: {
+        options: {
+          script: 'server.js'
+        }
+      },
+      prod: {
+        options: {
+          script: 'server.js',
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
+    casper: {
+      acceptance : {
+        options : {
+          test : true,
+        },
+        files : {
+          'test/acceptance/casper-results.xml' : ['test/acceptance/*_test.js']
+        }
+      }
     }
     
   });
   
-  grunt.registerTask('default','jshint');
+  grunt.registerTask('server', [ 'jshint', 'express:dev' ]);
+  grunt.registerTask('test',['express:dev','casper']);
+  grunt.registerTask('default', ['jshint', 'test']);
 };
