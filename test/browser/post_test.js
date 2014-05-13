002fa317d0409a26d854d62ff1d4bd988ff4b54c
Ivan Storck <ivanoats@gmail.com>
2014-05-13 15:20:05 -0700

    mocha from class

---
+++ test/browser/post_test.js
@@ -0,0 +1,25 @@
+var expect = chai.expect
+
+describe('Post object tests', function() {
+  var post;
+  
+  beforeEach (function() {
+    post = new Post('A test post');
+  });
+  
+  describe ('constructor', function() {
+    
+    it ('post should be truthy (exists)', function() {
+      expect(post).to.be.ok;
+    });
+    
+    it ('post should have title property', function() {
+      expect(post).to.have.property('title');
+    });
+    
+    it ('post title property matches beforeEach', function() {
+      expect(post.title).to.equal('A test post');
+    });
+    
+  });
+});
